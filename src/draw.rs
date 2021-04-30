8d28253d6b7bbff13c792ef25b178d4f00a32363
shanks-red <lezharayman@gmail.com>
2021-04-30 22:28:24 +0000

    edited the screen size to fit my screen

--- src/draw.rs
+++ src/draw.rs
@@ -1,14 +1,14 @@
 use piston_window::{rectangle, Context, G2d};
 use piston_window::types::Color;
 
-const BLOCK_SIZE : f64 = 25.0;
+const BLOCK_SIZE : f64 = 15.0;
 
 pub fn to_coord(game_coord : i32) -> f64
 {
-    game_coord as f64 * BLOCK_SIZE
+    (game_coord as f64) * BLOCK_SIZE
 }
 
-pub fn to_coord_32(game_coord : i32) -> u32
+pub fn to_coord_u32(game_coord : i32) -> u32
 {
     to_coord(game_coord) as u32
 }
