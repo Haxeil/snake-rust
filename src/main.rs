f49494cb37008b7754c0eca55b530e36b579975a
shanks-red <lezharayman@gmail.com>
2021-04-30 22:29:04 +0000

    organized code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-
+extern crate piston_window;
+extern crate rand;
 
 mod draw;
 mod snake;
@@ -7,42 +8,35 @@ mod game;
 use piston_window::*;
 use piston_window::types::Color;
 
-use game::Game;
-use draw::to_coord_32;
 
-const BLACK_COLOR : Color = [0.5, 0.5, 0.5, 1.0];
+use crate::game::Game;
+use crate::draw::to_coord_u32;
 
-fn main() 
-{
+const BACK_COLOR: Color = [0.5, 0.5, 0.5, 1.0];
 
-    let (width, height) = (20, 20);
+fn main() {
+    let (width, height) = (30, 30);
 
-    let mut window : PistonWindow = WindowSettings::new(
-        "Snake", 
-        [to_coord_32(width), to_coord_32(height)])
+    let mut window: PistonWindow =
+        WindowSettings::new("Snake", [to_coord_u32(width), to_coord_u32(height)])
             .exit_on_esc(true)
-                .build()
-                .unwrap();
-    
-    let mut game = Game::new(width, height);
+            .build()
+            .unwrap();
 
-    while let Some(event) = window.next()
-    {
-        if let Some(Button::Keyboard(key)) = event.press_args()
-        {
+    let mut game = Game::new(width, height);
+    while let Some(event) = window.next() {
+        if let Some(Button::Keyboard(key)) = event.press_args() {
             game.key_pressed(key);
         }
-        window.draw_2d(&event, |c, g|
-        {
-            clear(BLACK_COLOR, g);
+        window.draw_2d(&event, |c, g, _| {
+            clear(BACK_COLOR, g);
             game.draw(&c, g);
         });
+
         event.update(|arg| {
             game.update(arg.dt);
         });
     }
-
-
 }
 
 
